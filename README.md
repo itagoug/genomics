83a6be1ef3c26c876f22d112c25c9c1819c77117
Ines Tagoug <inestagoug@gmail.com>
2019-07-31 16:13:10 -0400

    CNV_calling

--- README.md
+++ README.md
@@ -3,14 +3,16 @@
 Scripts found in this repo will start analyses on __genomic data__ (fasta, fastq, bam, bed). Most tools used are well referenced in the field and used in bioinformatics in general.
 
 Genome analysis software will:
-- align and convert FASQT to bam (samtools).
-- remove duplicate reads (agilent sureselect).
-- check coverage (Qualimap).
-- Variant calling (Varscan2, bcftools and strelka2).
+- align and convert FASQT to bam.
+- remove duplicate reads for SureSelect custom capture.
+- check coverage.
+- Variant calling (SNVs, indels).
+- Germline filtering analysis.
+-Gene and variant annotation
 
 ## Testing
 
-Runnings tests were performed on PBS and Slurm.
+Runnings tests were performed on PBS and Slurm on H4H cluster at UHN, Toronto Canada.
 
 #strelka2:
 Strelka2 is a fast and accurate small variant caller optimized for analysis of germline variation in small cohorts and somatic variation in tumor/normal sample pairs.
---
+++ cnv_calling.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=6,walltime=48:00:00,vmem=30g,mem=220g
+#PBS -N calling
+#PBS -q all
+#PBS -V
+#PBS -j oe
+#PBS -m abe
+#PBS -M inestagoug@gmail.com
+
+
+set -eux
+
+module load java/8 bwa/0.7.15 samtools/1.9
+module load snpEff/4.3
+module load python3/3.4.3
+module load CNVkit/0.9.3
+
+## H4H directories
+scratch=/cluster/projects/tiedemannlab/itagoug
+raw=$scratch/sickkids1/bam_clean/dup_clean
+
+
+## __DO NOT CHANGE__
+pbs=$(echo $PBS_JOBID | cut -f1 -d '.')
+bamfiles=$scratch/sickkids1/bamfiles
+home=/cluster/home/itagoug
+user_databases=$scratch/databases
+workdir=$scratch/sickkids1/analysis/$pbs
+human_ref_genome=$scratch/human/lite/GRCh37-lite.fa
+bam_clean=$scratch/sickkids1/bam_clean
+mills=$scratch/human/gold/Mills.gold.standard.adapted_noChr.vcf
+agilent=$scratch/human/agilent_original/74_clean.bed
+snpeff_config=$home/scripts/snpEff.config
+
+
+mkdir -p $workdir
+cd $workdir
+
+	cnvkit.py batch $raw/206_P6.final.bam --normal $raw/206_Bu.final.bam \
+		  --targets $agilent \
+		  --fasta $human_ref_genome \
+		  --short-names \
+		  --output-reference $workdir/my_reference.cnn \
+		  --diagram $workdir/output.2.cnv.txt
+
+
+echo -e "\n\nAnalyses are done, without complaints."
